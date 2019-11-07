2582b931414fa65007a789c6067b79456984f9e8
Jineesh <johnj15@dixonscarphone.com>
2019-11-07 12:02:26 +0000

    Make api call from nextjs

--- nextjs/src/pages/index.js
+++ nextjs/src/pages/index.js
@@ -1,7 +1,19 @@
 import Nav from './nav';
-const Index = () => <div>
+import fetch from 'isomorphic-unfetch';
+const Index = (props) => <div>
     <Nav />
-    <h3>This is index page</h3>
+    <h3>This is index page:::: {props.bpi.time.updated} </h3>
 </div>;
+
+Index.getInitialProps = async() => {
+
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+    return {
+        bpi: data
+    }
+};
+
+
 
 export default Index;
